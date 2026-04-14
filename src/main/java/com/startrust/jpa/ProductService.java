3c01fc7383c0e1ac3412ed7124470246498064d2
WebbYang <webbyang@startrust.com.tw>
2026-04-14 21:32:44 +0800

    uuid version

--- src/main/java/com/startrust/jpa/ProductService.java
+++ src/main/java/com/startrust/jpa/ProductService.java
@@ -1,7 +1,5 @@
 package com.startrust.jpa;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
